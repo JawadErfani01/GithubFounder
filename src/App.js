3bda11d794d2e76f58375cad2a2f84d3e5ef0516
Jawad Erfani <jawad.erfani147@gmail.com>
2022-05-31 09:15:34 +0430

    daisyui added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
       <h1 className="text-5xl text-cyan-500 text-center font-bold p-10 italic">
         Hello world
       </h1>
+      <button className="btn btn-primary">Remove</button>
     </div>
   )
 }
